99a6586d0d77c88cd24a8de3abb70e5237a41397
WILMER <wilmer.vega@correo.tdea.edu.co>
2026-02-28 19:27:20 -0500

    comentario

---
+++ wilmer-vega/src/test/java/gestiondeunbanco/wilmer_vega/WilmerVegaApplicationTests.java
@@ -0,0 +1,13 @@
+package gestiondeunbanco.wilmer_vega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WilmerVegaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
