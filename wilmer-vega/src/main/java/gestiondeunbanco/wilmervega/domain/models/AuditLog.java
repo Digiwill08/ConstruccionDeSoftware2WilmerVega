23149e0e5dfa2fc32c732a2beff277980cb523e1
WILMER <wilmer.vega@correo.tdea.edu.co>
2026-03-05 19:36:48 -0500

    refactor: Simplify AuditLog model by removing JPA annotations and unnecessary fields

--- wilmer-vega/src/main/java/gestiondeunbanco/wilmervega/domain/models/AuditLog.java
+++ wilmer-vega/src/main/java/gestiondeunbanco/wilmervega/domain/models/AuditLog.java
@@ -3,39 +3,23 @@ package gestiondeunbanco.wilmervega.domain.models;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
-import jakarta.persistence.*;
 import java.sql.Timestamp;
 import java.util.Map;
 
-@Entity
-@Table(name = "audit_logs")
+
 @Getter
 @Setter
 @NoArgsConstructor
 public class AuditLog {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long auditLogId;
 
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
     private OperationType operationType;
-
-    @Column(nullable = false)
     private Timestamp operationDateTime;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
-    private SystemUser user;
-
-    @Column(nullable = false, length = 50)
+    private Long userId;
     private String userRole;
-
-    @Column(nullable = false, length = 50)
     private String affectedProductId;
-
-    @Column(nullable = false, columnDefinition = "JSON")
-    private String detailDataJson;
+    private Map<String, Object> detailData;
 
 
 }
+
