496061c1c6d4b8947453f54a311ae6ff7b7f3f94
WILMER <wilmer.vega@correo.tdea.edu.co>
2026-03-12 19:30:40 -0500

    refactor: Añadir atributos a la clase Usuario para mejorar la representación del usuario

--- wilmer-vega/src/main/java/gestiondeunbanco/wilmervega/domain/models/Usuario.java
+++ wilmer-vega/src/main/java/gestiondeunbanco/wilmervega/domain/models/Usuario.java
@@ -4,8 +4,28 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.sql.Date;
+
 @Getter
 @Setter
 @NoArgsConstructor
 public class Usuario extends Persona {
+
+	private Long userId;
+
+	private NaturalClient relatedClient;
+
+	private String fullName;
+
+	private String documentNumber;
+
+	private Date birthDate;
+
+	private SystemRole systemRole;
+
+	private UserStatus userStatus;
+
+	private String username;
+
+	private String password;
 }
